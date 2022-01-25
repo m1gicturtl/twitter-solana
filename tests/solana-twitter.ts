5b6a07850ea0cf9522cd26d4c320d2544dab503c
m1gicturtl <pernichevm@gmail.com>
2022-01-26 09:25:08 +1030

    add 'can send a tweet' test

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -2,16 +2,27 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { SolanaTwitter } from '../target/types/solana_twitter';
 
-describe('solana-twitter', () => {
+describe('solana_twitter', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it('can send a new tweet', async () => {
+    // Before sending the transaction to the blockchain.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('solana', '$sol @ $40 is probably a decent buy', {
+        accounts: {
+            tweet: tweet.publicKey,
+            author: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [tweet],
+    });
+
+    // After sending the transaction to the blockchain.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+    console.log(tweetAccount);
   });
 });
